08b71b2187f4f84e2b2ceedeb0b0025968b11327
Shane Moodie <shanepmoodie96@gmail.com>
2017-11-02 23:40:20 -0400

    Initial Commit

---
+++ WebApplication3/Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+        public string ReviewTitle { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string ReviewText { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
